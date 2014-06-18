3fbc88eeff78ed82e179d1e723e345dfb740bdd3
cdonaldson <cdonaldson@mobiquityinc.com>
2014-06-17 20:04:09 -0400

    done

--- video/test-video.js
+++ video/test-video.js
@@ -14,10 +14,10 @@ after (function(done){done();
         		"title": "JSON Syntax",
         		"description": "This is a description for the JSON Syntax video!  Yeeeeee haw",
         		"url": "https://SpiderLMS.s3.amazonaws.com/1234LKJ",
-        		"uploaded_by": 2,
+        		//"uploaded_by": 2,
         		"created_at": "2014-03-17 10:10:10",
         		"updated_at": "2014-03-17 10:10:10",
-        		"tags": "tag1, tag2, tag3, tag4",
+        		"tags": "tag1, tag2, tag3, tag4, tags5",
             	})
 
             .expect(201)
@@ -28,6 +28,14 @@ after (function(done){done();
                 }
                 else{ log.info('test 1 response: ' +JSON.stringify(res.body));
                 assert.equal(res.statusCode, 201);
+                assert.equal(res.body.id, 2);
+                assert.equal(res.body.title, "Updating the video title!");
+                assert.equal(res.body.description, "This is a description for the Spidey callbacks video.  yay.");
+                assert.equal(res.body.url,"https://SpiderLMS.s3.amazonaws.com/12345");
+                assert.equal(res.body.tags,"tag1, tag2, tag3, tag4, tags5");
+                assert.equal(res.body.created_at, "2014-03-17 10:10:10");
+                assert.equal(res.body.updated_at, "2014-03-17 10:10:10")
+
 
                 done();
             };
@@ -70,13 +78,41 @@ it('Test1: Retrieve a video - get request', function(done){
 
                 }
                 else{ log.info('test 2 response: ' +JSON.stringify(res.body));
-               log.info('test 2 Response code:'+res.statusCode);
-               assert.equal}
+                	log.info('test 2 Response code:'+res.statusCode);
+	                assert.equal(res.body.id, 2);
+   		            assert.equal(res.body.title, "Javascript Callbacks");
+   	    	        assert.equal(res.body.description, "This is a description for the Spidey callbacks video.  yay.");
+	       		    assert.equal(res.body.url,"https://SpiderLMS.s3.amazonaws.com/23234234LSKDJFSDF");
+	                assert.equal(res.body.uploaded_by, 2);
+   		            assert.equal(res.body.tags,"tag1, tag2, tag3, tag4");
+   		            assert.equal(res.body.created_at, "2014-03-17 10:10:10");
+   		            assert.equal(res.body.updated_at, "2014-03-17 10:10:10");
+   		            // assert.equal(res.body.assets,["assets, id: 1,  title: the slide show, filetype: pptx, fileurl:"http://this.is.an.amazon.s3.url "] )
+	   	            //var firstAsset = rest.body.assets[0].id;
+	   	            assert.equal(res.body.assets[0].id, 1);
+	   	            assert.equal(res.body.assets[0].title, "The slide show");
+	   	            assert.equal(res.body.assets[0].filetype, "pptx");
+	   	            assert.equal(res.body.assets[0].fileurl, "http://this.is.an.amazon.s3.url");
+	   	            assert.equal(res.body.assets[1].id, 2);
+	   	            assert.equal(res.body.assets[1].title, "The notes");
+	   	            assert.equal(res.body.assets[1].filetype, "pdf");
+	   	            assert.equal(res.body.assets[1].fileurl, "http://this.is.an.amazon.s3.url");
+	   	           
 
-                done();
-            });
+   	             	//console.log(firstAsset +JSON.stringify()
+   		            // console.log("First asset: " + JSON.stringify(firstAsset, null, 4));
 
-    });
+
+// // 
+//                assert.equal(res.body.assets, )
+// "assets" :  [{  "id" : 1, "title" : "The slide show", "filetype" : "pptx", "fileurl" : "http://this.is.an.amazon.s3.url" }, 
+//{ "id" : 2,  "title" : "The notes", "filetype" : "pdf", "fileurl" : "http://this.is.an.amazon.s3.url" }]}
+
+                	done();
+            	}
+        	});
+
+    	});
 
     it('Test3: Remove a Video - DELETE request', function(done){
         TheAPI
@@ -90,9 +126,11 @@ it('Test1: Retrieve a video - get request', function(done){
                 }
                 else{ log.info('test 2 response: ' +JSON.stringify(res.body));
                log.info('test 2 Response code:'+res.statusCode);
-               assert.equal}
+               // assert.equal}
                 
                 done();
+            }
             });
     });
-// });
+
+
